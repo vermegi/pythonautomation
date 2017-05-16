d69cf691a6cb9f312f0a7907df24692e36fb669c
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2017-05-17 00:04:52 +0200

    stuff

--- App_Data/jobs/continuous/pythoncontinuous/run.py
+++ App_Data/jobs/continuous/pythoncontinuous/run.py
@@ -1,9 +1,10 @@
 import os
 import platform
 
-message = "Using Python '{0}'".format(platform.python_version())  
-print(message)  
+msg = "Using Python '{0}'".format(platform.python_version())  
+print(msg)  
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'env')))
 from azure.storage.queue import QueueService
 
 print(os.environ['storagekey'])
@@ -11,7 +12,11 @@ print(os.environ['storagekey'])
 queue_service = QueueService(account_name='gytryout', account_key=os.environ['storagekey'])
 
 messages = queue_service.get_messages('webjobqueue')
+print("messages")
 for message in messages:
     print(message.content)
-    queue_service.delete_message('webjobqueue', message.id, message.pop_receipt)
+    for property in vars(message):
+        print(property)
+    #queue_service.delete_message('webjobqueue', message.id, message.pop_receipt)
 
+print("done")
