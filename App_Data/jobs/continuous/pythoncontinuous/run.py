eddc585eeb891e778a20f9cd23cab484d85e59fc
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2017-05-16 23:07:22 +0200

    added config

--- App_Data/jobs/continuous/pythoncontinuous/run.py
+++ App_Data/jobs/continuous/pythoncontinuous/run.py
@@ -4,14 +4,14 @@ import platform
 message = "Using Python '{0}'".format(platform.python_version())  
 print(message)  
 
-#from azure.storage.queue import QueueService
+from azure.storage.queue import QueueService
 
 print(os.environ['storagekey'])
 
-#queue_service = QueueService(account_name='gytryout', account_key=os.environ['storagekey'])
+queue_service = QueueService(account_name='gytryout', account_key=os.environ['storagekey'])
 
-#messages = queue_service.get_messages('webjobqueue')
-#for message in messages:
-#    print(message.content)
-#    queue_service.delete_message('webjobqueue', message.id, message.pop_receipt)
+messages = queue_service.get_messages('webjobqueue')
+for message in messages:
+    print(message.content)
+    queue_service.delete_message('webjobqueue', message.id, message.pop_receipt)
 
